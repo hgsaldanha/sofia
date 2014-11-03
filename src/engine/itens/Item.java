25e88401a1f42b272191fca3d57cfb58a2e251a8
alunoruy <alunoruy@rpt-ws-011478.acad.devrybrasil.com.br>
2014-11-03 19:45:15 -0200

    Ajuste de encerramento da thread na função moverPara

--- src/engine/itens/Item.java
+++ src/engine/itens/Item.java
@@ -176,35 +176,37 @@ public abstract class Item implements Runnable {
 
             @Override
             public void run() {
-                int x = getX();
-                int y = getY();
-                int w = dx - getX() ;
-                int h = dy - getY() ;
-                int dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0 ;
-                if (w<0) dx1 = -1 ; else if (w>0) dx1 = 1 ;
-                if (h<0) dy1 = -1 ; else if (h>0) dy1 = 1 ;
-                if (w<0) dx2 = -1 ; else if (w>0) dx2 = 1 ;
-                int longest = Math.abs(w) ;
-                int shortest = Math.abs(h) ;
-                if (!(longest>shortest)) {
-                    longest = Math.abs(h) ;
-                    shortest = Math.abs(w) ;
-                    if (h<0) dy2 = -1 ; else if (h>0) dy2 = 1 ;
-                    dx2 = 0 ;            
-                }
-                int numerator = longest >> 1 ;
-                for (int i=0;i<=longest;i++) {
-                    pausar(intervalo);
-                    setX(x);
-                    setY(y);
-                    numerator += shortest ;
-                    if (!(numerator<longest)) {
-                        numerator -= longest ;
-                        x += dx1 ;
-                        y += dy1 ;
-                    } else {
-                        x += dx2 ;
-                        y += dy2 ;
+                if (!GameController.getInstance().isFimJogo()) {
+                    int x = getX();
+                    int y = getY();
+                    int w = dx - getX() ;
+                    int h = dy - getY() ;
+                    int dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0 ;
+                    if (w<0) dx1 = -1 ; else if (w>0) dx1 = 1 ;
+                    if (h<0) dy1 = -1 ; else if (h>0) dy1 = 1 ;
+                    if (w<0) dx2 = -1 ; else if (w>0) dx2 = 1 ;
+                    int longest = Math.abs(w) ;
+                    int shortest = Math.abs(h) ;
+                    if (!(longest>shortest)) {
+                        longest = Math.abs(h) ;
+                        shortest = Math.abs(w) ;
+                        if (h<0) dy2 = -1 ; else if (h>0) dy2 = 1 ;
+                        dx2 = 0 ;            
+                    }
+                    int numerator = longest >> 1 ;
+                    for (int i=0;i<=longest && !GameController.getInstance().isFimJogo();i++) {
+                        pausar(intervalo);
+                        setX(x);
+                        setY(y);
+                        numerator += shortest ;
+                        if (!(numerator<longest)) {
+                            numerator -= longest ;
+                            x += dx1 ;
+                            y += dy1 ;
+                        } else {
+                            x += dx2 ;
+                            y += dy2 ;
+                        }
                     }
                 }
             }
